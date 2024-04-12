f54ef0e14f3a8b66b416adb04dcf607a30c71ff0
Pascal Hanke <pascal.hanke@yahoo.de>
2024-04-12 18:01:08 +0200

    uncommented all providers

--- src/components/Provider/Provider.tsx
+++ src/components/Provider/Provider.tsx
@@ -26,15 +26,15 @@ export type Offer = {
 };
 
 const fetchUrlByProvider: { [key in ProviderT]?: string } = {
-    // WBM: "wbm",
-    // FRIEDRICHSHEIM: "friedrichsheim",
-    // GEWOBAG: "gewobag",
-    // DEUTSCHE_WOHNEN: "deutschewohnen",
-    // HOWOGE: "howoge",
+    WBM: "wbm",
+    FRIEDRICHSHEIM: "friedrichsheim",
+    GEWOBAG: "gewobag",
+    DEUTSCHE_WOHNEN: "deutschewohnen",
+    HOWOGE: "howoge",
     DPF: "dpf",
-    // STADTUNDLAND: "stadtundland",
-    // GESOBAU: "gesobau",
-    // DAGEWO: "dagewo",
+    STADTUNDLAND: "stadtundland",
+    GESOBAU: "gesobau",
+    DAGEWO: "dagewo",
     //IMMOSCOUT: "immoscout",
 };
 
