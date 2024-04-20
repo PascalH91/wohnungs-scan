f9260a5f056256cc9d5f00afea340735a200974e
Pascal Hanke <p.hanke@p6o.de>
2024-04-20 18:04:36 +0200

    automatically locate chrome

--- src/utils/getBrowser.ts
+++ src/utils/getBrowser.ts
@@ -1,5 +1,6 @@
 import chromium from "@sparticuz/chromium";
 import puppeteerCore from "puppeteer-core";
+import locateChrome from "locate-chrome";
 
 chromium.setHeadlessMode = true;
 chromium.setGraphicsMode = false;
@@ -8,28 +9,13 @@ const CHROMIUM_PATH =
     "https://vomrghiulbmrfvmhlflk.supabase.co/storage/v1/object/public/chromium-pack/chromium-v123.0.0-pack.tar";
 
 export const getBrowser = async () => {
-    if (process.env.VERCEL_ENV === "production") {
-        const chromium = await import("@sparticuz/chromium-min").then((mod) => mod.default);
+    const executablePath: string | undefined = await new Promise((resolve) => locateChrome((arg: any) => resolve(arg)));
+    console.log("executablePath", executablePath);
 
-        const puppeteerCore = await import("puppeteer-core").then((mod) => mod.default);
-
-        const executablePath = await chromium.executablePath(CHROMIUM_PATH);
-
-        const browser = await puppeteerCore.launch({
-            args: ["--no-sandbox"],
-            // defaultViewport: chromium.defaultViewport,
-            executablePath,
-            headless: true,
-        });
-        return browser;
-    } else {
-        // const puppeteer = await import("puppeteer").then((mod) => mod.default);
-        // const browser = await puppeteer.launch();
-        const browser = await puppeteerCore.launch({
-            executablePath: "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
-            headless: true,
-            dumpio: true,
-        });
-        return browser;
-    }
+    const browser = await puppeteerCore.launch({
+        executablePath,
+        args: ["--no-sandbox", "--disable-setuid-sandbox"],
+        headless: true,
+    });
+    return browser;
 };
