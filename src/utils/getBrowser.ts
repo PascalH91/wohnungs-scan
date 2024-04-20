561de5804cdfd1f45b82a19e1bbb0c3aa0886717
Pascal Hanke <p.hanke@p6o.de>
2024-04-20 19:27:43 +0200

    puppeteer

--- src/utils/getBrowser.ts
+++ src/utils/getBrowser.ts
@@ -1,6 +1,7 @@
 import chromium from "@sparticuz/chromium";
 import puppeteerCore from "puppeteer-core";
 import locateChrome from "locate-chrome";
+import puppeteer from "puppeteer";
 
 chromium.setHeadlessMode = true;
 chromium.setGraphicsMode = false;
@@ -9,13 +10,21 @@ const CHROMIUM_PATH =
     "https://vomrghiulbmrfvmhlflk.supabase.co/storage/v1/object/public/chromium-pack/chromium-v123.0.0-pack.tar";
 
 export const getBrowser = async () => {
-    const executablePath: string | undefined = await new Promise((resolve) => locateChrome((arg: any) => resolve(arg)));
-    console.log("executablePath", executablePath);
+    // const executablePath: string | undefined = await new Promise((resolve) => locateChrome((arg: any) => resolve(arg)));
+    // console.log("executablePath", executablePath);
 
-    const browser = await puppeteerCore.launch({
-        executablePath,
-        args: ["--no-sandbox", "--disable-setuid-sandbox"],
-        headless: true,
+    // const browser = await puppeteerCore.launch({
+    //     executablePath,
+    //     args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    //     headless: true,
+    // });
+
+    const browser = await puppeteer.launch({
+        args: chromium.args,
+        defaultViewport: chromium.defaultViewport,
+        executablePath: await chromium.executablePath(),
+        headless: chromium.headless,
     });
+
     return browser;
 };
