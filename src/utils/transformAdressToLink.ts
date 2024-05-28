5fc8192af89cf2c097d4a4f4da024e6fd4c6e4d4
Pascal Hanke <p.hanke@p6o.de>
2024-05-28 15:27:22 +0200

    transformed adress-link

--- src/utils/transformAdressToLink.ts
+++ src/utils/transformAdressToLink.ts
@@ -1,6 +1,6 @@
 export const transformAddressToGoogleMapsLink = (address: string) => {
     const googleMapsBaseUrl = "https://www.google.com/maps/search/?api=1&loading=async&query=";
 
-    const transformedAddress = address.split(" ").join("+").split(",").join("%2C");
+    const transformedAddress = address.split(" ").join("+").split("\n").join("+").split(",").join("%2C");
     return googleMapsBaseUrl + transformedAddress;
 };
